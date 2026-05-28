e51b24e14f9ba8f856cdd2b966393a8cc2b4cfae
sahra <sahra-2000@hotmail.co.uk>
2026-05-28 06:21:21 +0100

    populate deck

--- src/main/java/org/example/CardGame.java
+++ src/main/java/org/example/CardGame.java
@@ -2,19 +2,31 @@ package org.example;
 import java.util.ArrayList;
 public class CardGame {
     public ArrayList<Card> deckOfCards;
-    private String name;
+    public String name;
 
-    public CardGame(ArrayList<Card> deckOfCards) {
-
-        this.deckOfCards = deckOfCards;
+    public CardGame(String name) {
+        this.name = name;
+        this.deckOfCards = new ArrayList<>();
+        populateDeck();
     }
+    public void populateDeck(){
+        String[] suits = {"♥", "♣", "♦", "♠"};
+        String[] symbols = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        int[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
+        for (String suit : suits) {
+            for (int i = 0; i < symbols.length; i++) {
+                Card card = new Card(suit, symbols[i], values[i]);
+                deckOfCards.add(card);
+            }
+        }
+    }
     public ArrayList<Card> getDeck() {
         return deckOfCards;
     }
     public Card dealCard(){
         if (!deckOfCards.isEmpty()) {
-            return deckOfCards.removeFirst(); // returns and removes card.
+            return deckOfCards.remove(0); // returns and removes card.
         }
 
         return null;
