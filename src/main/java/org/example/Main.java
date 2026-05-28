9b525f5bfc643147a5fad752d43db98a4c95b0e0
sahra <sahra-2000@hotmail.co.uk>
2026-05-28 06:42:28 +0100

    main check to see if deck has been populated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,9 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+        CardGame game = new CardGame("Test");
 
-        Card myCard = new Card("♦", "Q", 12);
-        System.out.println(myCard);
-
+        System.out.println(game.getDeck().size());
 
     }
 }
