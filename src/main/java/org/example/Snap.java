06af3f3799d61656ba09b09870b2553958535924
sahra <sahra-2000@hotmail.co.uk>
2026-05-29 09:52:38 +0100

    timer logic + try/catch

--- src/main/java/org/example/Snap.java
+++ src/main/java/org/example/Snap.java
@@ -9,6 +9,21 @@ public class Snap extends CardGame {
         String name = scanner.nextLine();
         return new Player(name);
     }
+    public boolean attemptSnap(Scanner scanner) {
+       try {
+        long startTime = System.currentTimeMillis();
+
+        String answer = scanner.nextLine();
+
+        long reactionTime = System.currentTimeMillis() - startTime;
+
+        return answer.equalsIgnoreCase("snap") &&
+                reactionTime <= 2000;
+       } catch (Exception e) {
+           System.out.println("Something went wrong.");
+           return false;
+       }
+    }
     public void start() {
 
         Scanner scanner = new Scanner(System.in);
@@ -17,7 +32,6 @@ public class Snap extends CardGame {
         Player playerTwo = createPlayer(scanner, "Enter Player 2 name:");
 
         shuffleDeck();
-
 
         Card lastCard = null;
         int turn = 0;
@@ -38,7 +52,13 @@ public class Snap extends CardGame {
                 System.out.println("Card dealt: " + activeCard);
 
                 if (lastCard != null && activeCard.getSymbol().equals(lastCard.getSymbol())) {
-                    System.out.println("SNAP! Congratulations, " + activePlayer.getName() + " wins!");
+                    System.out.println("SNAP! Type 'snap' within 2 seconds!");
+                    if (attemptSnap(scanner)) {
+                        System.out.println("SNAP! Congratulations, " + activePlayer.getName() + " wins!");
+                    }
+                    else {
+                        System.out.println("Sorry, "+ activePlayer.getName() + " was too slow!");
+                    }
                     break;
                 }
                 lastCard = activeCard;
