ec137df62c4a5a2e8b3e094ddd340a6c616ee280
sahra <sahra-2000@hotmail.co.uk>
2026-05-29 13:32:41 +0100

    clean up

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class Player {
-        private  String name;
+        private final String name;
 
         public Player(String name) {
                 this.name = name;
