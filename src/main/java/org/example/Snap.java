526d95ba0cf3d48f26631b2bc3e759307c7e41dd
sahra <sahra-2000@hotmail.co.uk>
2026-05-29 02:55:09 +0100

    Snap Logic

--- src/main/java/org/example/Snap.java
+++ src/main/java/org/example/Snap.java
@@ -4,5 +4,17 @@ public class Snap extends CardGame {
     public Snap(String name) {
         super(name);
     }
-   // public void start() {
+    public void start() {
+        Card lastCard = null;
+        while (true) {
+            Card activeCard = dealCard();
+            System.out.println(activeCard);
+            if (lastCard != null && activeCard.getSymbol().equals(lastCard.getSymbol())) {
+                System.out.println("SNAP! Congratulations, You Win!");
+                break;
+            }
+            lastCard = activeCard;
+        }
+    }
 }
+
